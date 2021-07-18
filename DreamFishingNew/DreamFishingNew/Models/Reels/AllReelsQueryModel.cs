23730ac825cf28fb4f4fefb372860c86b531f97d
VentsislavAleksandrov <Veenn3@gmail.com>
2021-07-18 13:11:02 +0300

    Implemented AllReelQueryModel, ReelSortingEnum, and ReelListingViewModel

---
+++ DreamFishingNew/DreamFishingNew/Models/Reels/AllReelsQueryModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DreamFishingNew.Models.Reels
+{
+    public class AllReelsQueryModel
+    {
+    }
+}
