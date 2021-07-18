cae1bb3dcdd2204c1b7897ddb22112bdbd9ad52d
VentsislavAleksandrov <Veenn3@gmail.com>
2021-07-18 17:12:20 +0300

    Implemented GlassesSorting, GlassesListingViewModel, AllGlassesQueryModel

--- DreamFishingNew/DreamFishingNew/Controllers/ClothesController.cs
+++ DreamFishingNew/DreamFishingNew/Controllers/ClothesController.cs
@@ -2,6 +2,7 @@
 using DreamFishingNew.Data.Models;
 using DreamFishingNew.Models.Clothes;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 namespace DreamFishingNew.Controllers
@@ -15,9 +16,69 @@ namespace DreamFishingNew.Controllers
             this.data = data;
         }
 
-        public IActionResult All()
+        public IActionResult All([FromQuery]AllClothesQueryModel query)
         {
-            return View();
+            var clothesQuery = data.Clothes
+                .Include<Clothes>("Brand")
+                .ToList();
+
+            if (!string.IsNullOrWhiteSpace(query.Brand))
+            {
+                clothesQuery = data.Clothes
+                    .Where(x => x.Brand.Name.ToLower() == query.Brand)
+                    .ToList();
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.SearchTerm))
+            {
+                clothesQuery = clothesQuery
+                    .Where(x => (x.Brand.Name + " " + x.Model).ToLower().Contains(query.SearchTerm.ToLower())
+                    ||x.Description.ToLower().Contains(query.SearchTerm.ToLower())
+                    )
+                    .ToList();
+            }
+
+            clothesQuery = query.Sorting switch
+            {
+                ClothesSorting.MinPrice => clothesQuery.OrderBy(x => x.Price).ToList(),
+                ClothesSorting.MaxPrice => clothesQuery.OrderByDescending(x => x.Price).ToList(),
+                ClothesSorting.BrandAndModel => clothesQuery.OrderBy(x => x.Brand.Name).ThenBy(x => x.Model).ToList(),
+                _ => clothesQuery.OrderBy(x => x.Brand.Name).ThenBy(x => x.Model).ToList()
+            };
+
+            var clothes = clothesQuery
+                .Skip((query.currentPage -1) * AllClothesQueryModel.ClothesPerPage)
+                .Take(AllClothesQueryModel.ClothesPerPage)
+                .Select(x => new ClothesListingViewModel
+                {
+                    Id = x.Id,
+                    Model = x.Model,
+                    Brand = x.Brand.Name,
+                    Image = x.Image,
+                    Price = x.Price,
+                })
+                .ToList();
+
+            
+
+
+            var clothesBrands = data
+                .Clothes
+                .Select(x => x.Brand.Name)
+                .Distinct()
+                .ToList();
+
+
+            var model = new AllClothesQueryModel
+            {
+                Brand = query.Brand,
+                Brands = clothesBrands,
+                Clothes = clothes,
+                Sorting = query.Sorting,
+                SearchTerm = query.SearchTerm,
+            };
+
+            return View(model);
         }
 
         public IActionResult Add()
--- DreamFishingNew/DreamFishingNew/Models/Clothes/AllClothesQueryModel.cs
+++ DreamFishingNew/DreamFishingNew/Models/Clothes/AllClothesQueryModel.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DreamFishingNew.Models.Clothes
 {
     public class AllClothesQueryModel
     {
+        public const int ClothesPerPage = 6;
+
+        public string Brand { get; set; }
+
+        public IEnumerable<string> Brands { get; set; }
+
+        [Display(Name ="Search")]
+        public string SearchTerm { get; set; }
+
+        public ClothesSorting Sorting { get; set; }
+
+        public int currentPage { get; set; } = 1;
+
+        public IEnumerable<ClothesListingViewModel> Clothes { get; set; }
     }
 }
