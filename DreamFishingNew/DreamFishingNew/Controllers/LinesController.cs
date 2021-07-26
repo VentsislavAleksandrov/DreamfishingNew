57bdfd9eb1ac4d158734b408ad67d0a781407399
VentsislavAleksandrov <Veenn3@gmail.com>
2021-07-26 15:21:54 +0300

    Implemented Edit for Lines

--- DreamFishingNew/DreamFishingNew/Controllers/LinesController.cs
+++ DreamFishingNew/DreamFishingNew/Controllers/LinesController.cs
@@ -146,5 +146,63 @@ namespace DreamFishingNew.Controllers
 
             return View(model);
         }
+        public IActionResult Edit(int id)
+        {
+            var model = data.Lines
+                .Where(x => x.Id == id)
+                .Select(x => new AddLineFormModel 
+                {              
+                Model = x.Model,
+                Brand = x.Brand.Name,
+                Length = x.Length,
+                Size = x.Size,
+                Weight = x.Weight,
+                Description = x.Description,
+                Image = x.Image,
+                Price = x.Price,
+                Quantity = x.Quantity
+                })
+                .FirstOrDefault();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, AddLineFormModel item)
+        {
+            var brand = data.Brands.FirstOrDefault(x => x.Name.ToLower() == item.Brand.ToLower());
+
+            if (brand == null)
+            {
+                this.ModelState.AddModelError(nameof(item.Brand), "Brand does not exist.");
+            }
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(item);
+            }
+
+
+            var bait = data
+                .Lines
+                .Include("Brand")
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            bait.Model = item.Model;
+            bait.Brand.Name = item.Brand;
+            bait.Length = item.Length;
+            bait.Description = item.Description;
+            bait.Image = item.Image;
+            bait.Size = item.Size;
+            bait.Price = item.Price;
+            bait.Quantity = item.Quantity;
+            bait.Weight = item.Weight;
+
+            data.SaveChanges();
+
+            return RedirectToAction("All", "Lines");
+        }
     }
 }
